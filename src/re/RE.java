08d2f2677755a6236a91a86fce1998aa27ed7588
Matt Mecham <msmecham.dev@gmail.com>
2022-12-03 10:41:02 -0700

    Added some comments

--- src/re/RE.java
+++ src/re/RE.java
@@ -2,6 +2,16 @@ package re;
 
 import fa.nfa.NFA;
 
+// Our alphabet: { a, b }
+// General CFG for this regex
+// regex:   term '|' regex
+//          | term
+// term:    { factor } "possibly empty sequence of factors"
+// factor:  base '*'
+//          | base
+// base:    char
+//          | '(' regex ')'
+
 public class RE implements re.REInterface {
 
     private String inputString;
@@ -12,21 +22,33 @@ public class RE implements re.REInterface {
         nfa = new NFA();
     }
 
+    //TODO: Method needs to account for a '|" situation
+    // Needs to appropriately call NFA class methods to create
+    // a state that has an OR transition, basically. I am not
+    // positive on parameter types
     private NFA choice(NFA thisOne, NFA thatOne) {
 
         return nfa;
     }
 
+    //TODO: Method needs to account for when when first state leads to just another
+    // Possibly like, aabb.. Might go something like 0 -a-> 1 -a-> 2 or something like that
+    // Not sure atm if a sequence is also something like (a|b)a where (a|b) is "first" and
+    // a is "second"... Probably, though?
     private NFA sequence(NFA first, NFA second) {
 
         return nfa;
     }
 
+    //TODO: Method needs to account for '*' case
     private NFA repetition(NFA internal) {
 
         return nfa;
     }
 
+    //TODO: The linked algorithm has this and not quite sure what to do with it.
+    // Perhaps how we know the name of the transition. This is the most basic piece
+    // or the recursive parse.
     private NFA primitive(char c)
     {
 
@@ -38,6 +60,9 @@ public class RE implements re.REInterface {
         return ret;
     }
 
+    //The lead terminal of our CFG
+    //Regex:    term '|' regex
+    //          | term
     private NFA regex() {
         NFA termNFA = term();
 
@@ -48,13 +73,12 @@ public class RE implements re.REInterface {
         } else {
             return termNFA;
         }
-
     }
 
+    // Next level of CFG:
+    // term:    { factor } "Possibly empty sequence of factors"
     private NFA term() {
-        //needs to be static constant... not quite sure how to implement atm
-        //Could possibly just be a blank node or something
-        NFA factorNFA = null;
+        NFA factorNFA = null; //Since it could possibly be empty? Not sure about this one
 
         while (more() && peek() != ')' && peek() != '|') {
             NFA nextFactorNFA = factor();
@@ -63,6 +87,9 @@ public class RE implements re.REInterface {
         return factorNFA;
     }
 
+    // Next level of CFG:
+    // factor:  base '*'
+    //          | base
     private NFA factor() {
         NFA baseNFA = base();
 
@@ -73,6 +100,9 @@ public class RE implements re.REInterface {
         return baseNFA;
     }
 
+    // Final level of CFG:
+    // base:    char
+    //          | '(' regex ')'
     private NFA base() {
 
         switch (peek()) {
